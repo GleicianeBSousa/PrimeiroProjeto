4108dd03b6e514ec01135309f552a2d58dd97ab0
GleicianeBSousa <gleiciane_bsousa@hotmail.com>
2024-05-11 18:23:15 -0300

    exercicio

---
+++ ExercíciosNovos/ex01.py
@@ -0,0 +1,7 @@
+'''
+     Crie um programa que leia quanto dinheiro em reais uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
+'''
+
+real = int(input("Quantos R$ você possui? "))
+dolar = real * 5.14
+print("Você possui em real R${} e em dólar ${}".format(real,dolar))
